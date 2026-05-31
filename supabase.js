bab2fc092f7e68f899fa5980a0837b4b39fba59f
nsoformiracle20-creator <nsoformiracle20@gmail.com>
2026-05-31 17:32:38 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,2 +1,8 @@
-zoPDM9gRHntFgml3_PRF1Q_AuNXomLD
+const SUPABASE_URL = "YOUR_PROJECT_URL";
+const SUPABASE_ANON_KEY = "YOUR_PUBLISHABLE_KEY";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
 
